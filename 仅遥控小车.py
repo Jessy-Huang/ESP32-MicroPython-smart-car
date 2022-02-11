07bc9c6905ed0d68f92163af6a8fac81e696eb0f
Jessy-Huang <66520966+Jessy-Huang@users.noreply.github.com>
2022-02-11 14:56:04 +0800

    Add files via upload

--- 仅遥控小车.py
+++ 仅遥控小车.py
@@ -1,61 +1,81 @@
 from machine import Pin, PWM
+from time import sleep
 import machine
-from utime import sleep
 from myIRremote import IR
 
-if __name__ == "__main__":
-    machine.freq(160000000)
-    pwm1 = PWM(Pin(13), freq=500, duty=0)
-    pwm2 = PWM(Pin(27), freq=500, duty=0)
-    pwm3 = PWM(Pin(12), freq=500, duty=0)
-    pwm4 = PWM(Pin(14), freq=500, duty=0)
+# 设置管脚PIN
+left1_pin  = 13   
+left2_pin   = 12
+right1_pin  = 14   #5
+right2_pin  = 27
+IRremote_pin = 34
+
+def motor_setup():
+    global motro_left1
+    global motro_left2
+    global motro_right1     
+    global motro_right2
+        
+    motro_left1 = PWM(Pin(left1_pin), freq=20000, duty=0)  # 创建motor pwm对象，设置为输出模式 
+    motro_left2 = PWM(Pin(left2_pin),freq=20000, duty=0)
+    motro_right1 = PWM(Pin(right1_pin),freq=20000, duty=0)   
+    motro_right2 = PWM(Pin(right2_pin),freq=20000, duty=0)
+# 方向控制
+def slow_forward():
+    motro_left1.duty(780) # 输出高电平
+    motro_left2.duty(0)
+    motro_right1.duty(780) # 输出高电平
+    motro_right2.duty(0)
+def turn_left():
+    motro_left1.duty(380) # 输出高电平 400
+    motro_left2.duty(0)
+    motro_right1.duty(680) # 输出高电平
+    motro_right2.duty(0)
+def turn_right():
+    motro_left1.duty(680) # 输出高电平
+    motro_left2.duty(0)
+    motro_right1.duty(380) # 输出高电平
+    motro_right2.duty(0)
+def backward():
+    motro_left1.duty(0) # 输出高电平
+    motro_left2.duty(780)
+    motro_right1.duty(0) # 输出高电平
+    motro_right2.duty(780)
+def stop():
+    motro_left1.duty(0) # 输出高电平
+    motro_left2.duty(0)
+    motro_right1.duty(0) # 输出高电平
+    motro_right2.duty(0)
+# 程序入口
+if __name__ == '__main__':
+    motor_setup()
+    ir = IR(IRremote_pin)
     state = 0
-    speed = 512
-    t = IR(34)
-    while(True):
-        changed, s, repeat, t_ok = t.scan()
-        if changed == True:
-            if s == '1':
-                speed = 512
-            elif s == '2':
-                speed = 640
-            elif s == '3':
-                speed = 768
-            if repeat >= 2:
-                if s == 'up' and state != 1:
-                    state = 1
-                    pwm1.duty(speed)       
-                    pwm2.duty(speed)        
-                    pwm3.duty(0)          
-                    pwm4.duty(0)          
-                    print("state1")
-                elif s == 'down' and state != 2:
-                    state = 2
-                    pwm1.duty(0)          
-                    pwm2.duty(0)          
-                    pwm3.duty(speed)        
-                    pwm4.duty(speed)        
-                    print("state2")
-                elif s == 'left' and state != 3:
-                    state = 3
-                    pwm1.duty(0)          
-                    pwm2.duty(int(speed/2 + 128))        
-                    pwm3.duty(int(speed/2 + 128))        
-                    pwm4.duty(0)         
-                    print("state3")
-                elif s == 'right' and state != 4:
-                    state = 4
-                    pwm1.duty(int(speed/2 + 128))        
-                    pwm2.duty(0)          
-                    pwm3.duty(0)          
-                    pwm4.duty(int(speed/2 + 128))        
-                    print("state4")
+    
+    while True:
+        changed, s, repeat, ir_ok = ir.scan()
+        if changed == True:  
+            if s == 'up' and state != 1:
+                state = 1
+                slow_forward()
+                print("forward")
+            elif s == 'down' and state != 2:
+                state = 2
+                backward()
+                print("backward")
+            elif s == 'left' and state != 3:
+                state = 3
+                turn_left()
+                print("turn left")
+            elif s == 'right' and state != 4:
+                state = 4
+                turn_right()
+                print("turn right")
         else:
             if state != 0:
                 state = 0
-                pwm1.duty(0)         
-                pwm2.duty(0)         
-                pwm3.duty(0)          
-                pwm4.duty(0)         
-                print("state0")
+                stop()
+                print("stop")
         sleep(0.2)
+                
+        
